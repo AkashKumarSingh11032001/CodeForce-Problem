3a0c2681415c748290c546d71ac298a1e40196c4
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-03-25 19:39:36 +0530

    Done

---
+++ Beginner/Nearly_Lucky_Number.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+bool check(int x)
+{
+    if (x == 4 || x == 7)
+    {
+        return true;
+    }
+    
+}
+
+int main()
+{
+    long long n;
+    cin >> n;
+    int count = 0;
+    while (n != 0)
+    {
+        if (n % 10 == 4 || n % 10 == 7)
+        {
+            count += 1;
+        }
+        n /= 10;
+    }
+    if (count == 4 || count == 7)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+    return 0;
+}
