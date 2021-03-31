008ce7ce97152fb98ad96e9d41e19b7582fcc6e1
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-03-31 20:55:08 +0530

    Done

--- Beginner/I_Wanna_Be_the_Guy.cpp
+++ Beginner/I_Wanna_Be_the_Guy.cpp
@@ -1,20 +1,41 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+int main()
+{
+    int array[250];
+    int n, p, q;
+    cin >> n >> p;
+    int count = 0;
 
-    int n;
-    cin >> n;
-
-    int p;
-    cin >> p;
-    int arr[p];
     for (int i = 0; i < p; i++)
     {
-        /* code */
-        cin >> arr[i];
+        cin >> array[i];
+    }
+    cin >> q;
+    for (int i = p; i < p + q; i++)
+    {
+        cin >> array[i];
     }
 
-    
-    
+    sort(array, array + (p + q));
+
+    for (int i = 0; i < p + q; i++)
+    {
+        if (array[i] != array[i + 1])
+        {
+            count++;
+        }
+    }
+
+    if (n == (count))
+    {
+        cout << "I become the guy." << endl;
+    }
+    else
+    {
+        cout << "Oh, my keyboard!" << endl;
+    }
+
+    return 0;
 }
